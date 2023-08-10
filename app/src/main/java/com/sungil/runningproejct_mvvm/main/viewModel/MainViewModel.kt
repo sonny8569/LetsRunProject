8f6ff3eeacddfb17c4bfe3a4252cdeb13ca982d5
user <sonny0204@gmail.com>
2023-08-11 00:10:43 +0900

    Main쪽 구현

--- app/src/main/java/com/sungil/runningproejct_mvvm/main/viewModel/MainViewModel.kt
+++ app/src/main/java/com/sungil/runningproejct_mvvm/main/viewModel/MainViewModel.kt
@@ -6,7 +6,11 @@ import com.sungil.runningproejct_mvvm.utill.ListenerMessage
 import com.sungil.runningproejct_mvvm.utill.RepositoryListener
 
 class MainViewModel(private val repository: MainRepository) : ViewModel() , RepositoryListener {
+<<<<<<< HEAD
 //    val wearLiveData get() = repository.getRunningRoomDB()
+=======
+    val wearLiveData get() = repository.getRunningRoomDB()
+>>>>>>> 3a7f4ee (Main쪽 구현)
     //repository
     override fun onMessageSuccess(data: ListenerMessage) {
         TODO("Not yet implemented")
