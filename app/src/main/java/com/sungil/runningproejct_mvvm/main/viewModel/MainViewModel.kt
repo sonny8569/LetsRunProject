39a4e76dd3cff07b9cd72b47a08ebc91e4cace3b
user <sonny0204@gmail.com>
2023-08-16 00:38:34 +0900

    Appdatabase 기존 -> 직접 싱글톤 으로 구현 현제 -> hilt 적용

--- app/src/main/java/com/sungil/runningproejct_mvvm/main/viewModel/MainViewModel.kt
+++ app/src/main/java/com/sungil/runningproejct_mvvm/main/viewModel/MainViewModel.kt
@@ -7,10 +7,14 @@ import com.sungil.runningproejct_mvvm.utill.RepositoryListener
 
 class MainViewModel(private val repository: MainRepository) : ViewModel() , RepositoryListener {
 <<<<<<< HEAD
+<<<<<<< HEAD
 //    val wearLiveData get() = repository.getRunningRoomDB()
 =======
     val wearLiveData get() = repository.getRunningRoomDB()
 >>>>>>> 3a7f4ee (Main쪽 구현)
+=======
+//    val wearLiveData get() = repository.getRunningRoomDB()
+>>>>>>> 905652d (Appdatabase 기존 -> 직접 싱글톤 으로 구현 현제 -> hilt 적용)
     //repository
     override fun onMessageSuccess(data: ListenerMessage) {
         TODO("Not yet implemented")
