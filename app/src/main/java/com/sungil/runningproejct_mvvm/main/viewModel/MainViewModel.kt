9eb6530fe10fb6b9d541c7f75b2b21be7d4bbdb1
user <sonny0204@gmail.com>
2023-08-27 01:30:54 +0900

    Activity follower -> vieModel follower 로 변경

--- app/src/main/java/com/sungil/runningproejct_mvvm/main/viewModel/MainViewModel.kt
+++ app/src/main/java/com/sungil/runningproejct_mvvm/main/viewModel/MainViewModel.kt
@@ -1,18 +1,25 @@
 package com.sungil.runningproejct_mvvm.main.viewModel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sungil.runningproejct_mvvm.dataObject.FirebasePostData
 import com.sungil.runningproejct_mvvm.repository.MainRepository
 import com.sungil.runningproejct_mvvm.utill.ListenerMessage
 import com.sungil.runningproejct_mvvm.utill.RepositoryListener
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 //MainViewModel inject
 @HiltViewModel
-class MainViewModel @Inject constructor  (private val repository: MainRepository ): ViewModel() , RepositoryListener {
+class MainViewModel @Inject constructor  (private val repository: MainRepository ): ViewModel()  {
     //repository
     var wearLiveData = repository.getRunningRoomDB()
 
-    var followerLiveData =repository.getFollowerLiveData()
+    var followerLiveData = MutableLiveData<List<String>>()
 
     var followerPostLiveData = repository.getFollowerPostLiveData()
 
@@ -21,10 +28,17 @@ class MainViewModel @Inject constructor  (private val repository: MainRepository
     var setFollowerLiveData = repository.getSetFollowerLiveData()
 
     private var follower = ArrayList<String>()
-    fun getFollower(){
+
+    val postLiveData = MutableLiveData<String>()
+
+    fun getFollower() = viewModelScope.launch(Dispatchers.IO) {
         val data = repository.getUserInfo()
         if (data != null) {
-            repository.getFollower(data.id)
+            val followers = repository.getFollower(data.id)
+            followers.collect {
+                follower.addAll(it)
+                getFollowerPost()
+            }
         }
     }
 
@@ -38,18 +52,28 @@ class MainViewModel @Inject constructor  (private val repository: MainRepository
 
     fun writeNewFollower(userId : String){
         val data = repository.getUserInfo() ?: return
+        if(follower.contains(userId)){
+            Timber.e("Already Follower User")
+            return
+        }
         repository.setNewFollower(userId , data.id)
     }
     fun setFollower(followers : ArrayList<String>){
         this.follower = followers
     }
 
-    override fun onMessageSuccess(data: ListenerMessage) {
-        TODO("Not yet implemented")
+    fun postSns() = viewModelScope.launch(Dispatchers.IO) {
+
+        val userInfo = repository.getUserInfo()
+        var data = FirebasePostData(
+            userInfo!!.id, "test2입니다", "test2", 1692804242263, "123km", userInfo.nickName
+        )
+
+        val result = repository.writePost(data)
+        result.collect {
+            postLiveData.postValue(it)
+        }
     }
 
-    override fun onMessageFail(data: ListenerMessage) {
-        TODO("Not yet implemented")
-    }
 
 }
