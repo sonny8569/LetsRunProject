2ba990628b02a4d0d583624823471c77e156a1e4
user <sonny0204@gmail.com>
2023-08-29 00:29:28 +0900

    usecase 1:1 매칭

--- app/src/main/java/com/sungil/runningproejct_mvvm/useCase/UseCase.kt
+++ app/src/main/java/com/sungil/runningproejct_mvvm/useCase/UseCase.kt
@@ -4,29 +4,8 @@ import com.google.firebase.database.DataSnapshot
 import com.sungil.runningproejct_mvvm.dataObject.FirebasePostData
 import javax.inject.Inject
 
-class UseCase @Inject constructor() {
-
-    fun getFollowerData(snapshot : DataSnapshot , follower : ArrayList<String>): ArrayList<FirebasePostData> {
-        val followerMap = snapshot.value as? HashMap<String ,HashMap<String ,Any>>
-        val followerPost = ArrayList<FirebasePostData>()
-        followerMap?.forEach { (email, innerMap) ->
-            if (follower.contains(email)) {
-                // innerMap에서 필요한 데이터 추출
-                val runningDistance = innerMap["running"]?.toString()?.trim() ?: ""
-                val runningTime = innerMap["time"] as? Long ?: 0L
-                val title = innerMap["title"] as? String ?: ""
-                val content = innerMap["content"] as? String ?: ""
-                val nickname = innerMap["nickName"] as? String ?: ""
-                // PostData 객체 생성
-                val firebasePostData = FirebasePostData(email , title, content, runningTime, runningDistance , nickname)
-                followerPost.add(firebasePostData)
-            }
-        }
-        return followerPost
-
-    }
-
-    fun getUnFollowerData(snapshot : DataSnapshot , follower : ArrayList<String>): ArrayList<FirebasePostData> {
+class GetUnFollowerUseCase @Inject constructor() {
+    fun getUnFollowerData(snapshot : DataSnapshot, follower : ArrayList<String>): ArrayList<FirebasePostData> {
         val unFollowerMap = snapshot.value as? HashMap<String, HashMap<String, Any>>
         val unFollowerPost = ArrayList<FirebasePostData>()
         unFollowerMap?.forEach { (email, innerMap) ->
