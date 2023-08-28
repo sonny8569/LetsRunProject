095586adf89f7245595833a6b23d63d5f65a5514
user <sonny0204@gmail.com>
2023-08-28 23:34:27 +0900

    확장함수로 변경

--- app/src/main/java/com/sungil/runningproejct_mvvm/utill/PostAdapter.kt
+++ app/src/main/java/com/sungil/runningproejct_mvvm/utill/PostAdapter.kt
@@ -5,6 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.sungil.runningproejct_mvvm.dataObject.FirebasePostData
 import com.sungil.runningproejct_mvvm.databinding.AdapterPostBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
 
 class PostAdapter() : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
@@ -13,14 +17,15 @@ class PostAdapter() : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
             field = value
             notifyDataSetChanged()
         }
-    val commUtil  = CommUtil()
-    private var setOnClickListener : SetOnClickListener  ?= null
-    inner class ViewHolder(private val binding : AdapterPostBinding) : RecyclerView.ViewHolder(binding.root){
-        fun bind(data : FirebasePostData){
-            binding.txtWriter.text= data.nickName
+    private var setOnClickListener: SetOnClickListener? = null
+
+    inner class ViewHolder(private val binding: AdapterPostBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: FirebasePostData) {
+            binding.txtWriter.text = data.nickName
             binding.txtTitle.text = data.title
             binding.txtContent.text = data.content
-            binding.txtTime.text = commUtil.convertLocalTime(data.time)
+            binding.txtTime.text = data.time.convertLocalTime()
             binding.txtRunData.text = data.running
 
             binding.layoutRoot.setOnClickListener {
@@ -31,7 +36,7 @@ class PostAdapter() : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = AdapterPostBinding.inflate(LayoutInflater.from(parent.context) ,parent , false )
+        val binding = AdapterPostBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
@@ -43,8 +48,16 @@ class PostAdapter() : RecyclerView.Adapter<PostAdapter.ViewHolder>() {
         holder.bind(data[position])
     }
 
-    fun setOnClickListener(listener : SetOnClickListener){
+    fun setOnClickListener(listener: SetOnClickListener) {
         this.setOnClickListener = listener
     }
 
+    private fun Long.convertLocalTime(): String {
+        val timeZone = TimeZone.getDefault()
+        val format = SimpleDateFormat("MM-dd HH:mm", Locale.getDefault())
+        format.timeZone = timeZone
+
+        val date = Date(this)
+        return format.format(date)
+    }
 }
