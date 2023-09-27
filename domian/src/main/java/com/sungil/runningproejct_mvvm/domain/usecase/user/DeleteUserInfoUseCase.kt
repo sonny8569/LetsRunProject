ab0dcf5f91c878a070ae461500876ccbaa49adb4
user <sonny0204@gmail.com>
2023-09-27 10:15:31 +0900

    domain runningRepo 추가 유저 저장정보 삭제 usecase 추가

---
+++ domian/src/main/java/com/sungil/runningproejct_mvvm/domain/usecase/user/DeleteUserInfoUseCase.kt
@@ -0,0 +1,12 @@
+package com.sungil.runningproejct_mvvm.domain.usecase.user
+
+import com.sungil.runningproejct_mvvm.domain.entity.UserInfoData
+import com.sungil.runningproejct_mvvm.domain.interactor.UserInfoRepository
+import javax.inject.Inject
+
+class DeleteUserInfoUseCase @Inject constructor(private val userInfoRepository: UserInfoRepository) {
+
+    fun deleteUserInfo(data: UserInfoData) {
+        userInfoRepository.deleteUserInfo(data)
+    }
+}
