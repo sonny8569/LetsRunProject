2e60eab4dc228aa567d4026c918be19b274b166d
user <sonny0204@gmail.com>
2023-10-05 23:42:35 +0900

    기존 UserInfoData 및 RunningRate interface 을 Controller Interface 로 통합 유저삭제 버전

--- domian/src/main/java/com/sungil/runningproejct_mvvm/domain/usecase/user/DeleteUserInfoUseCase.kt
+++ domian/src/main/java/com/sungil/runningproejct_mvvm/domain/usecase/user/DeleteUserInfoUseCase.kt
@@ -1,10 +1,11 @@
 package com.sungil.runningproejct_mvvm.domain.usecase.user
 
 import com.sungil.runningproejct_mvvm.domain.entity.UserInfoData
+import com.sungil.runningproejct_mvvm.domain.interactor.ControllerRepository
 import com.sungil.runningproejct_mvvm.domain.interactor.UserInfoRepository
 import javax.inject.Inject
 
-class DeleteUserInfoUseCase @Inject constructor(private val userInfoRepository: UserInfoRepository) {
+class DeleteUserInfoUseCase @Inject constructor(private val userInfoRepository: ControllerRepository) {
 
     fun deleteUserInfo(data: UserInfoData) {
         userInfoRepository.deleteUserInfo(data)
