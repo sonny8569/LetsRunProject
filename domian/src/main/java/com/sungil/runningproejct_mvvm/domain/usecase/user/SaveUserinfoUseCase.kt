a95d37368986de11c61e63e135cec8b00618c226
user <sonny0204@gmail.com>
2023-10-05 23:41:39 +0900

    기존 UserInfoData 및 RunningRate interface 을 Controller Interface 로 통합 추가

--- domian/src/main/java/com/sungil/runningproejct_mvvm/domain/usecase/user/SaveUserinfoUseCase.kt
+++ domian/src/main/java/com/sungil/runningproejct_mvvm/domain/usecase/user/SaveUserinfoUseCase.kt
@@ -1,10 +1,11 @@
 package com.sungil.runningproejct_mvvm.domain.usecase.user
 
 import com.sungil.runningproejct_mvvm.domain.entity.UserInfoData
+import com.sungil.runningproejct_mvvm.domain.interactor.ControllerRepository
 import com.sungil.runningproejct_mvvm.domain.interactor.UserInfoRepository
 import javax.inject.Inject
 
-class SaveUserinfoUseCase @Inject constructor(private val userInfoRepository: UserInfoRepository) {
+class SaveUserinfoUseCase @Inject constructor(private val userInfoRepository: ControllerRepository) {
 
     fun saveUserInfo(data: UserInfoData) {
         userInfoRepository.saveUserInfo(data)
